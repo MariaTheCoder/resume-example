e20ec3d1b435f71677459d0146d345fb86d59cea
Maria T. Andersen <mteilmann93@gmail.com>
2023-06-01 09:32:37 +0200

    npm install @fontsource-variable/outfit, import font and include in the styles of :root

--- main.js
+++ main.js
@@ -2,5 +2,6 @@
  * This project follows the YouTube tutorial "From Figma to Code /Creating a resume page" from user Kevin Powell
  * Link: https://www.youtube.com/watch?v=LkZPd0oRlMQ
  * */
+import "@fontsource-variable/outfit";
 
 console.log("This works!");
