d1eef3b89268e5ca35ccb84ccc170f4e0ddd35e6
Maria T. Andersen <mteilmann93@gmail.com>
2023-06-01 09:20:50 +0200

    styling of the root element. This style for font family, font weight, font sizing and colors are given directly from the YouTube tutorial, which is linked in the main.js file

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+/**
+ * This project follows the YouTube tutorial "From Figma to Code /Creating a resume page" from user Kevin Powell
+ * Link: https://www.youtube.com/watch?v=LkZPd0oRlMQ
+ * */
+
 console.log("This works!");
